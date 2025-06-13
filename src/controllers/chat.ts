98acae107b43e73da8163550607e3bb7dcbd4c07
naumanch969 <naumanch969@gmail.com>
2025-06-13 14:21:22 +0500

    Message history added in RAG pipeline

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express';
 import { getVectorStore } from '../config/init.js';
-import { RunnableSequence } from '@langchain/core/runnables';
+import { RunnablePassthrough, RunnableSequence } from '@langchain/core/runnables';
 import { convertDocsToString } from '../workers/helper.js';
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
 import { StringOutputParser } from "@langchain/core/output_parsers";
+import { MessagesPlaceholder } from "@langchain/core/prompts";
+import { RunnableWithMessageHistory } from "@langchain/core/runnables";
+import { ChatMessageHistory } from "langchain/stores/message/in_memory";
+
 
 export const queryPDF = async (req: Request, res: Response) => {
     try {
@@ -20,49 +24,83 @@ export const queryPDF = async (req: Request, res: Response) => {
             convertDocsToString
         ]);
 
-        // Answer Generation Prompt
-        const TEMPLATE_STRING = `You are an experienced researcher, 
-        expert at interpreting and answering questions based on provided sources.
-        Using the provided context, answer the user's question 
-        to the best of your ability using only the resources provided. 
-        Be verbose!
-
-        <context>
-
-        {context}
-
-        </context>
-
-        Now, answer this question using the above context:
-
-        {question}`;
-
-        const answerGenerationPrompt = ChatPromptTemplate.fromTemplate(
-            TEMPLATE_STRING
-        );
-
-
         // Initialize the model
         const model = new ChatGoogleGenerativeAI({ model: "gemini-1.5-pro", temperature: 0, maxRetries: 2, });
 
         // Initialize output parser
-        const outputParser = new StringOutputParser();
+        const stringOutputParser = new StringOutputParser();
 
-        // Chain to generate the answer
-        const retrievalChain = RunnableSequence.from([
-            {
-                context: contextRetrievalChain,
-                question: (input) => input.question,
-            },
-            answerGenerationPrompt,
-            model,
-            outputParser
+        // Rephrase question chain prompt
+        const REPHRASE_QUESTION_SYSTEM_TEMPLATE =
+            `Given the following conversation and a follow up question, 
+            rephrase the follow up question to be a standalone question.`;
+
+        const rephraseQuestionChainPrompt = ChatPromptTemplate.fromMessages([
+            ["system", REPHRASE_QUESTION_SYSTEM_TEMPLATE],
+            new MessagesPlaceholder("history"),
+            [
+                "human",
+                "Rephrase the following question as a standalone question:\n{question}"
+            ],
         ]);
 
-        // Run the chain with the query
-        const answer = await retrievalChain.invoke({ question: query });
+        // Rephrase question chain
+        const rephraseQuestionChain = RunnableSequence.from([
+            rephraseQuestionChainPrompt,
+            model,
+            stringOutputParser,
+        ])
 
-        res.json({ result: answer });
+        // Answer generation chain prompt
+        const ANSWER_CHAIN_SYSTEM_TEMPLATE = `You are an experienced researcher, 
+            expert at interpreting and answering questions based on provided sources.
+            Using the below provided context and chat history, 
+            answer the user's question to the best of 
+            your ability 
+            using only the resources provided. Be verbose!
+
+            <context>
+            {context}
+            </context>`;
+
+        const answerGenerationChainPrompt = ChatPromptTemplate.fromMessages([
+            ["system", ANSWER_CHAIN_SYSTEM_TEMPLATE],
+            new MessagesPlaceholder("history"),
+            [
+                "human",
+                "Now, answer this question using the previous context and chat history:\n{standalone_question}"
+            ]
+        ]);
+
+        // Conversational retrieval chain
+        const conversationalRetrievalChain = RunnableSequence.from([
+            RunnablePassthrough.assign({
+                standalone_question: rephraseQuestionChain,
+            }),
+            RunnablePassthrough.assign({
+                context: contextRetrievalChain,
+            }),
+            answerGenerationChainPrompt,
+            model,
+            stringOutputParser,
+        ]);
+
+        // Initialize message history - to store the conversation history of session
+        const messageHistory = new ChatMessageHistory();
+
+        // Final retrieval chain with message history
+        const finalRetrievalChain = new RunnableWithMessageHistory({
+            runnable: conversationalRetrievalChain,
+            getMessageHistory: (_sessionId) => messageHistory,
+            historyMessagesKey: "history",
+            inputMessagesKey: "question",
+        });
+
+        // Run the final retrieval chain with the query
+        const originalAnswer = await finalRetrievalChain.invoke({ question: query, }, { configurable: { sessionId: "test" } });
+        const followUpAnswer = await finalRetrievalChain.invoke({ question: "What did I just ask you?", }, { configurable: { sessionId: "test" } });
+
+        res.json({ result: originalAnswer, followUp: followUpAnswer });
     } catch (error) {
         console.error("Error in queryPDF:", error);
         res.status(500).json({ error: "Internal Server Error" });
